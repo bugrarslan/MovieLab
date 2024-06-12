3d5f4dcd200fb9518d5dd6a725084ea9dd1b4ffb
bugrarslan <bugrarsln7@gmail.com>
2024-06-12 15:21:48 +0300

    12.06

--- src/components/TrendingMovies.js
+++ src/components/TrendingMovies.js
@@ -10,21 +10,22 @@ export default function TrendingMovies({ data }) {
   const navigation = useNavigation();
 const handleClick = (item) => {
     navigation.push("Movie", item);
-
   };
 
   return (
     <View className="mb-8">
       <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
-      <Carousel
-        data={data}
-        renderItem={({ item }) => <MovieCard item={item} handleClick={handleClick}/>}
-        firstItem={1}
-        inactiveSlideOpacity={0.6}
-        itemWidth={width * 0.62}
-        sliderWidth={width}
-        slideStyle={{ display: "flex", alignItems: "center" }}
-      />
+        <Carousel
+          data={data}
+          renderItem={({ item }) => 
+            <MovieCard item={item} handleClick={handleClick}/>
+          }
+          firstItem={1}
+          inactiveSlideOpacity={0.6}
+          itemWidth={width * 0.62}
+          sliderWidth={width}
+          slideStyle={{ display: "flex", alignItems: "center" }}
+        />
     </View>
   );
 }
